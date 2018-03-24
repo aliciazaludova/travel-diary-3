c14cd2d3f1e6d9609f54faf5caf52b5c4b46d0af
Alicia Zaludova <aliciazaludova@gmail.com>
2018-03-23 22:01:42 -0500

    eventListener working but prints compound each time

--- main.js
+++ main.js
@@ -45,12 +45,35 @@ const buildDomString = (countriesArray) => {
         domString += `<h2>${country.location}</h2>`;
         domString += `<img src=${country.image}>`;
         domString += `<h3>${country.description}</h3>`;
-        domString += `<textarea placeholder="Begin entry..."></textarea>`;
+        domString += `<textarea class="diary-input" placeholder="Begin entry..."></textarea>`;
         domString += `<br>`;
-        domString += `<button class="card-button">Add diary entry</button>`;
+        domString += `<button class="card-button">Add entry</button>`;
         domString += `</div>`;
     });
     printToDom(domString, 'travel-diary');
 }
 buildDomString(travelDiary);
 
+// create variable to hold diary entry input that will become output
+let inputToOutput = document.getElementsByClassName("diary-input");
+
+// create empty array to hold the diary entry string so something is holding it before you call print
+domArray = [];
+
+// locate buttons
+const diaryButtons = document.getElementsByClassName('card-button');
+console.log('diaryEntryButtons:', diaryButtons);
+
+
+// listen for click on buttons--loop through each
+for (let i = 0; i < diaryButtons.length; i++) {
+    diaryButtons[i].addEventListener('click', (e) => {
+       domString = ''
+       domString += `<div class="output-card"`;
+       domString += `<h2>${travelDiary.location}</h2>`;
+       domString += `<textarea class="output">${inputToOutput[i].value}</textarea>`;
+       domString += `</div`;
+       domArray.push(domString);
+       printToDom(domArray, 'printed-entries');
+    });
+}
